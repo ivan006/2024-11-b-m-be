e7717b9fe4aed3b176dbebb2fc92bb62a6d59cea
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-20 09:44:52 +0200

    1

--- database/migrations/2024_11_20_074410_make_name_nullable_in_users_table.php
+++ database/migrations/2024_11_20_074410_make_name_nullable_in_users_table.php
@@ -6,23 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('name')->nullable()->change(); // Make the 'name' column nullable
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('name')->nullable(false)->change(); // Revert 'name' to NOT NULL
         });
     }
+
 };
