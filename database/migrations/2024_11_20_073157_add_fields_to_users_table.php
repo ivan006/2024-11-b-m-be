1cda70fe04ce5447246004631d30fcc7b14aaf70
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-20 09:35:25 +0200

    1

--- database/migrations/2024_11_20_073157_add_fields_to_users_table.php
+++ database/migrations/2024_11_20_073157_add_fields_to_users_table.php
@@ -6,23 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('company_name')->nullable(); // Nullable company name
+            $table->string('first_name')->nullable();   // Nullable first name
+            $table->string('last_name')->nullable();    // Nullable last name
+            $table->string('mobile_number')->nullable(); // Nullable mobile number
+            $table->string('profile_photo')->nullable(); // Nullable profile photo URL
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['company_name', 'first_name', 'last_name', 'mobile_number', 'profile_photo']);
         });
     }
+
 };
